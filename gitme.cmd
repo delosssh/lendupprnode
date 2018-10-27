c7c764d0de21dc89e405a7294a5503eb0eb0829a
delosssh <delosssh@gmail.com>
2018-10-27 19:57:59 +0300

    updated

--- gitme.cmd
+++ gitme.cmd
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "updated"
 git remote remove origin
-git remote add origin https://github.com/delosssh/lendupphilricenode.git
+git remote add origin https://github.com/delosssh/lendupprnode.git
 git push -u origin master
